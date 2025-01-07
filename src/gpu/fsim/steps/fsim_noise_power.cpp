d097790820c0883bf9da7800e09ab5d8eb15870e
Petr Volf <petr.volf216@gmail.com>
2025-01-07 20:00:13 +0100

    FSIM - much faster median compute

--- src/gpu/fsim/steps/fsim_noise_power.cpp
+++ src/gpu/fsim/steps/fsim_noise_power.cpp
@@ -1,11 +1,14 @@
 /*
  * Image Quality Metrics
- * Petr Volf - 2024
+ * Petr Volf - 2025
  */
 
 #include "fsim_noise_power.h"
 
 #include <fsim.h>
+
+#include <execution>
+#include <algorithm>
 
 IQM::GPU::FSIMNoisePower::FSIMNoisePower(const VulkanRuntime &runtime) {
     auto [buf, mem] = runtime.createBuffer(
@@ -16,6 +19,42 @@ IQM::GPU::FSIMNoisePower::FSIMNoisePower(const VulkanRuntime &runtime) {
     buf.bindMemory(mem, 0);
     this->noisePowers = std::move(buf);
     this->noisePowersMemory = std::move(mem);
+
+    this->kernel = runtime.createShaderModule("../shaders_out/fsim_pack_for_median.spv");
+
+    //custom layout for this pass
+    this->descSetLayout = std::move(runtime.createDescLayout({
+        vk::DescriptorSetLayoutBinding{
+            .binding = 0,
+            .descriptorType = vk::DescriptorType::eStorageBuffer,
+            .descriptorCount = 1,
+            .stageFlags = vk::ShaderStageFlagBits::eCompute,
+        },
+        vk::DescriptorSetLayoutBinding{
+            .binding = 1,
+            .descriptorType = vk::DescriptorType::eStorageBuffer,
+            .descriptorCount = 1,
+            .stageFlags = vk::ShaderStageFlagBits::eCompute,
+        },
+    }));
+
+    const std::vector layouts = {
+        *this->descSetLayout,
+    };
+
+    vk::DescriptorSetAllocateInfo descriptorSetAllocateInfo = {
+        .descriptorPool = runtime._descPool,
+        .descriptorSetCount = static_cast<uint32_t>(layouts.size()),
+        .pSetLayouts = layouts.data()
+    };
+
+    this->descSet = std::move(vk::raii::DescriptorSets{runtime._device, descriptorSetAllocateInfo}.front());
+
+    // 1x uint - buffer size
+    const auto ranges = VulkanRuntime::createPushConstantRange(sizeof(unsigned));
+
+    this->layout = runtime.createPipelineLayout({this->descSetLayout}, {ranges});
+    this->pipeline = runtime.createComputePipeline(this->kernel, this->layout);
 }
 
 void IQM::GPU::FSIMNoisePower::copyBackToGpu(const VulkanRuntime &runtime, const vk::raii::Buffer& stgBuf) {
@@ -49,6 +88,83 @@ void IQM::GPU::FSIMNoisePower::copyBackToGpu(const VulkanRuntime &runtime, const
 }
 
 void IQM::GPU::FSIMNoisePower::computeNoisePower(const VulkanRuntime &runtime, const vk::raii::Buffer& filterSums, const vk::raii::Buffer& fftBuffer, int width, int height) {
+    auto [bufTemp, memTemp] = runtime.createBuffer(
+        2 * FSIM_ORIENTATIONS * width * height * sizeof(float),
+        vk::BufferUsageFlagBits::eStorageBuffer | vk::BufferUsageFlagBits::eTransferSrc,
+        vk::MemoryPropertyFlagBits::eDeviceLocal
+    );
+    bufTemp.bindMemory(memTemp, 0);
+
+    vk::DescriptorBufferInfo bufInfoIn {
+        .buffer = fftBuffer,
+        .offset = 0,
+        .range = 2 * width * height * sizeof(float) * FSIM_ORIENTATIONS * FSIM_SCALES * 3,
+    };
+
+    vk::DescriptorBufferInfo bufInfoOut {
+        .buffer = bufTemp,
+        .offset = 0,
+        .range = width * height * sizeof(float) * FSIM_ORIENTATIONS * 2,
+    };
+
+    const vk::WriteDescriptorSet writeSetIn{
+        .dstSet = this->descSet,
+        .dstBinding = 0,
+        .dstArrayElement = 0,
+        .descriptorCount = 1,
+        .descriptorType = vk::DescriptorType::eStorageBuffer,
+        .pImageInfo = nullptr,
+        .pBufferInfo = &bufInfoIn,
+        .pTexelBufferView = nullptr,
+    };
+
+    const vk::WriteDescriptorSet writeSetOut{
+        .dstSet = this->descSet,
+        .dstBinding = 1,
+        .dstArrayElement = 0,
+        .descriptorCount = 1,
+        .descriptorType = vk::DescriptorType::eStorageBuffer,
+        .pImageInfo = nullptr,
+        .pBufferInfo = &bufInfoOut,
+        .pTexelBufferView = nullptr,
+    };
+
+    const std::vector writes = {
+        writeSetIn, writeSetOut,
+    };
+
+    runtime._device.updateDescriptorSets(writes, nullptr);
+
+    const vk::CommandBufferBeginInfo beginInfo = {
+        .flags = vk::CommandBufferUsageFlags{vk::CommandBufferUsageFlagBits::eOneTimeSubmit},
+    };
+    runtime._cmd_buffer->begin(beginInfo);
+
+    runtime._cmd_buffer->bindPipeline(vk::PipelineBindPoint::eCompute, this->pipeline);
+    runtime._cmd_buffer->bindDescriptorSets(vk::PipelineBindPoint::eCompute, this->layout, 0, {this->descSet}, {});
+
+    runtime._cmd_buffer->pushConstants<unsigned>(this->layout, vk::ShaderStageFlagBits::eCompute, 0, width * height);
+
+    auto groups = (width * height) / 256 + 1;
+
+    runtime._cmd_buffer->dispatch(groups, 1, 1);
+
+    runtime._cmd_buffer->end();
+
+    const std::vector cmdBufs = {
+        &**runtime._cmd_buffer
+    };
+
+    const vk::SubmitInfo submitInfo{
+        .commandBufferCount = 1,
+        .pCommandBuffers = *cmdBufs.data()
+    };
+
+    const vk::raii::Fence fence{runtime._device, vk::FenceCreateInfo{}};
+
+    runtime._queue->submit(submitInfo, *fence);
+    runtime.waitForFence(fence);
+
     auto [stgBuf, stgMem] = runtime.createBuffer(
         2 * FSIM_ORIENTATIONS * sizeof(float),
         vk::BufferUsageFlagBits::eTransferSrc | vk::BufferUsageFlagBits::eTransferDst,
@@ -67,7 +183,7 @@ void IQM::GPU::FSIMNoisePower::computeNoisePower(const VulkanRuntime &runtime, c
 
     auto * filterSumsCpu = static_cast<float*>(filterSumsCpuMem.mapMemory(0,  FSIM_ORIENTATIONS * sizeof(float), {}));
 
-    uint64_t largeBufSize = width * height * sizeof(float) * 2;
+    uint64_t largeBufSize = width * height * sizeof(float) * 2 * FSIM_ORIENTATIONS;
     auto [stgBufLarge, stgMemLarge] = runtime.createBuffer(
         largeBufSize,
         vk::BufferUsageFlagBits::eTransferSrc | vk::BufferUsageFlagBits::eTransferDst,
@@ -75,19 +191,16 @@ void IQM::GPU::FSIMNoisePower::computeNoisePower(const VulkanRuntime &runtime, c
     );
     stgBufLarge.bindMemory(stgMemLarge, 0);
     auto * bufDataLarge = static_cast<float*>(stgMemLarge.mapMemory(0, largeBufSize, {}));
-    std::vector<float> sortBuf(width * height);
 
+    this->copyFilterToCpu(runtime, bufTemp, stgBufLarge, width, height);
+
+    std::vector<float> sortBuf(width * height * 2 * FSIM_ORIENTATIONS);
+    memcpy(sortBuf.data(), bufDataLarge, largeBufSize);
     for (int i = 0; i < FSIM_ORIENTATIONS * 2; i++) {
-        this->copyFilterToCpu(runtime, fftBuffer, stgBufLarge, width, height, i);
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                float real = bufDataLarge[(x + width * y) * 2];
-                float imag = bufDataLarge[(x + width * y) * 2 + 1];
-                sortBuf[x + width * y] = real * real + imag * imag;
-            }
-        }
-        std::sort(sortBuf.begin(), sortBuf.end());
-        float median = sortBuf[sortBuf.size() / 2];
+        unsigned int start = i * (width * height);
+        unsigned int end = (i + 1) * (width * height);
+        std::sort(std::execution::par_unseq, sortBuf.begin() + start, sortBuf.begin() + end);
+        float median = sortBuf.at(start + (width * height) / 2);
         float mean = -median / std::log(0.5);
 
         bufData[i] = mean / filterSumsCpu[i % FSIM_ORIENTATIONS];
@@ -99,20 +212,20 @@ void IQM::GPU::FSIMNoisePower::computeNoisePower(const VulkanRuntime &runtime, c
     stgMem.unmapMemory();
 }
 
-void IQM::GPU::FSIMNoisePower::copyFilterToCpu(const VulkanRuntime &runtime, const vk::raii::Buffer& fftBuf, const vk::raii::Buffer& target, int width, int height, int index) {
+void IQM::GPU::FSIMNoisePower::copyFilterToCpu(const VulkanRuntime &runtime, const vk::raii::Buffer& tempBuf, const vk::raii::Buffer& target, int width, int height) {
     const vk::CommandBufferBeginInfo beginInfo = {
         .flags = vk::CommandBufferUsageFlags{vk::CommandBufferUsageFlagBits::eOneTimeSubmit},
     };
     runtime._cmd_buffer->begin(beginInfo);
 
-    uint64_t filterSize = width * height * sizeof(float) * 2;
+    uint64_t filterSize = width * height * sizeof(float) * 2 * FSIM_ORIENTATIONS;
 
     vk::BufferCopy region {
-        .srcOffset = filterSize * (FSIM_ORIENTATIONS * FSIM_SCALES + index * FSIM_SCALES),
+        .srcOffset = 0,
         .dstOffset = 0,
         .size = filterSize,
     };
-    runtime._cmd_buffer->copyBuffer(fftBuf, target, {region});
+    runtime._cmd_buffer->copyBuffer(tempBuf, target, {region});
 
     runtime._cmd_buffer->end();
 
